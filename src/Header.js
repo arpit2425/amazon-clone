067a5a187066eaa84261b9b10b54b3c0b8f89e8f
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-07-29 22:30:41 +0530

    Add search bar and icon in header

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,31 @@
-import React from 'react'
-import "./Header.css"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
+import './Header.css';
 function Header() {
-  return (
-    <nav className="header">
-      <img className="header__logo"
-       src="https://i.dlpng.com/static/png/197684_preview.png"
-        alt="logo"/>
-    </nav>
-  )
-}  
+    return (
+        <nav className='header'>
+            <Link to='/'>
+                <img
+                    className='header__logo'
+                    src='https://i.dlpng.com/static/png/197684_preview.png'
+                    alt='logo'
+                />
+            </Link>
+            <div className='header__search'>
+                <input type='text' className='header__searchInput' />
+                <SearchIcon className='header__searchIcon' />
+            </div>
+            <div className='header__nav'>
+                <Link className='header__link'>
+                    <div className='header_option'>
+                        <span>Hello Arpit</span>
+                        <span>Sign In</span>
+                    </div>
+                </Link>
+            </div>
+        </nav>
+    );
+}
 
-export default Header
+export default Header;
