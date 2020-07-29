98b665b70b3e3acefa5b895f8fe5e01ff4d40b99
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-07-29 21:57:08 +0530

    Add logo in header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
+import Header from './Header';
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                         <h1>Login</h1>
                     </Route>
                     <Route path='/'>
+                        <Header />
                         <h1>Home</h1>
                     </Route>
                 </Switch>
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import "./Header.css"
 function Header() {
   return (
     <nav className="header">
       <img className="header__logo"
-       src="https://pluspng.com/img-png/amazon-logo-vector-png-amazon-logo-vector-512.png"
+       src="https://i.dlpng.com/static/png/197684_preview.png"
         alt="logo"/>
     </nav>
   )
