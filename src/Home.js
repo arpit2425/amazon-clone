eaa131d2a8ac01dd2580b5c072c3254911caf1dd
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-07-29 23:57:50 +0530

    Add banner

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import './Home.css'
 function Home() {
   return (
-    <div>
-      Home page
-    </div>
+    <div className="home">
+    
+<img  className="home__banner"
+src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Wireless/Xiaomi/Redmi_Note9/GW/TOMO_TOD_ON/Updated/D17052101_IN_WLME_Redmi_Note9_LandingPage_DesktopTallHero_1500x600._CB409717819_.jpg"
+/>   
+</div>
   )
 }
 
